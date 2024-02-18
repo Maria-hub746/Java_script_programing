ef097718f336567163539759c476bd0eb7c97423
Mariia Volochniuk <mariyav14012010@gmail.com>
2024-02-18 17:34:00 +0200

    second hw

---
+++ hw_2/second_hw.js
@@ -0,0 +1,10 @@
+name = "Mariia"
+if (name==="Mariia") {
+	console.log("Hello me!");
+} else if (name==="Sasha") {
+	console.log("Hello Dad!");
+} else if (name==="Yana") {
+	console.log("Hello Mum!")
+} else {
+	console.log("Hello stranger!")
+}
